f8ea7560aac6199660754ae35ef452e87a70e5af
samirbhattarai135 <147074565+samirbhattarai135@users.noreply.github.com>
2024-12-18 08:47:50 -0600

    custom message

--- catkin_ws/src/tutorials/scripts/publisher_node.py
+++ catkin_ws/src/tutorials/scripts/publisher_node.py
@@ -2,13 +2,18 @@
 
 import rospy
 from std_msgs.msg import String
+from tutorials.msg import Position
 
 def publisher():
-    pub = rospy.Publisher('talking_topic', String, queue_size=10)
+    pub = rospy.Publisher('talking_topic', Position, queue_size=10)
     rospy.init_node('publisher_node', anonymous=True)
     rate = rospy.Rate(1)  # 1 Hz
+    rospy.loginfo("Publisher Node started, now publishing messages")
     while not rospy.is_shutdown():
-        msg = "Hello Samir - %s"% rospy.get_time()
+        msg = Position()
+        msg.message = "My Position is: "
+        msg.x = 2.0
+        msg.y = 1.5
         pub.publish(msg)
         rate.sleep()
 
--- catkin_ws/src/tutorials/scripts/subscriber_node.py
+++ catkin_ws/src/tutorials/scripts/subscriber_node.py
@@ -2,14 +2,15 @@
 
 import rospy
 from std_msgs.msg import String
+from tutorials.msg import Position
 
 
 def callback(data):
-    rospy.loginfo("Received data: %s", data.data)
+    rospy.loginfo("%s X: %f Y: %f", data.message, data.x, data.y)
 
 def subscriber():
     rospy.init_node("Subscriber_Node", anonymous=True)
-    rospy.Subscriber('talking_topic', String, callback)
+    rospy.Subscriber('talking_topic', Position, callback)
     rospy.spin()
 
 if __name__ == '__main__':
